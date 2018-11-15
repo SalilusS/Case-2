1b86a4993b177a5d4a13d1eec026632c1c7cbe35
SalilusS <44485874+SalilusS@users.noreply.github.com>
2018-11-15 22:48:58 +0700

    Update Main.py
    +inputs

--- Main.py
+++ Main.py
@@ -7,3 +7,11 @@ Sychov A. (), Virt A. (), Ostanina K. ()
 
 #TODOS:
 #TODO: Analysis
+
+print("Укажите ваш семейный статус:")
+print("(1) Один субъект")
+print("(2) Семейная пара")
+print("(3) Родитель-одиночка")
+status = int(input())
+print("Укажите ваш ежемесячный доход (в долларах):")
+salary = int(input())
